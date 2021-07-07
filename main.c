1b81f0c6bc3c224280fb14b095c0000bde091405
ali0gamer1 <69399548+ali0gamer1@users.noreply.github.com>
2021-07-07 23:18:19 +0430

    Update main.c

--- main.c
+++ main.c
@@ -3,66 +3,182 @@
 #include <inttypes.h>
 #include "utilsme.h"
 
+unsigned int address = 0x400000;
+#define PRINT_ADDRESS(heh) printf("%x:"heh,address-4)
 
-void detect_register(char _r1[], char r1)
+
+void detect_register(char _r1[], char *r1)
 {
-            if (r1 == 3 || r1 == 2){
+
+            if (*r1 == 3 || *r1 == 2){ //v0 v1
                 _r1[0] = 'v';
-                _r1[1] = (r1-2) + '0';
+                _r1[1] = ((*r1)-2) + '0';
             }else
 
 
-            if (r1 <= 7 && r1 >= 4){
+            if (*r1 <= 7 && *r1 >= 4){ // a0-a3
                 _r1[0] = 'a';
-                _r1[1] = (r1-4) + '0';
+                _r1[1] = (*r1-4) + '0';
 
             }else
 
-            if (r1 <= 15 && r1 >= 8){
+            if (*r1 <= 15 && *r1 >= 8){ //t0-t7
 
                 _r1[0] = 't';
-                _r1[1] = (r1-8) + '0';
+                _r1[1] = (*r1-8) + '0';
 
             }else
 
-            if (r1 <= 23 && r1 >= 16){
+            if (*r1 <= 23 && *r1 >= 16){ //s0-s7
 
                 _r1[0] = 's';
-                _r1[1] = (r1-16) + '0';
+                _r1[1] = (*r1-16) + '0';
 
             }else
 
-            if (r1 == 24 || r1 == 25){
+            if (*r1 == 24 || *r1 == 25){ //t8 t9
                 _r1[0] = 't';
-                _r1[1] = (r1-16) + '0';
+                _r1[1] = (*r1-16) + '0';
 
             }else
 
-            if (r1 == 26 || r1 == 27){
+            if (*r1 == 26 || *r1 == 27){ //k0 k1
                 _r1[0] = 'k';
-                _r1[1] = (r1-26) + '0';
+                _r1[1] = (*r1-26) + '0';
 
             }else
 
 
-            if (r1 == 28)
+            if (*r1 == 28)
                 strcpy(_r1, "gp");
             else
 
-            if (r1 == 29)
+            if (*r1 == 29)
                 strcpy(_r1, "sp");
             else
 
-            if (r1 == 30)
+            if (*r1 == 30)
                 strcpy(_r1, "fp");
             else
 
-            if (r1 == 31)
+            if (*r1 == 31)
                 strcpy(_r1, "ra");
+
+            else
+                *r1 = -1;
+
+}
+
+
+char detect_instruction(unsigned char bits)
+{
+    if (bits == 0)
+        return 0;
+
+    else
+    if (bits == 0b11 || bits ==0b10)
+        return 2;
+
+
+    return 1;
+}
+
+
+void parse_r(uint_fast32_t x)
+{
+    unsigned char func = getnbits(x,6,6);
+
+    char r1, r2, r3, sa;
+
+    char _r1[4] = {0}, _r2[4] = {0}, _r3[4] = {0};
+
+
+    sa = getnbits(x,11,5); //shift amount
+    r3 = getnbits(x,16,5); //rd
+    r2 = getnbits(x,21,5); //rt
+    r1 = getnbits(x,26,5); //rs
+
+    detect_register(_r1,&r1);
+    detect_register(_r2,&r2);
+    detect_register(_r3,&r3);
+
+
+
+   if (func == 0) //sll
+   {
+       if ( r2 == -1 || r3 == -1) //check if invalid register
+            return;
+        PRINT_ADDRESS("\t");
+       printf("sll\t%s, %s, 0x%x\n", _r3, _r2, sa);
+   }
+
+   if (func == 0x2) //sll
+   {
+       if ( r2 == -1 || r3 == -1) //check if invalid register
+            return;
+        PRINT_ADDRESS("\t");
+       printf("srl\t%s, %s, 0x%x\n", _r3, _r2, sa);
+   }
+
+   if (func == 0x3) //sll
+   {
+       if ( r2 == -1 || r3 == -1) //check if invalid register
+            return;
+
+       PRINT_ADDRESS("\t");
+       printf("sra\t%s, %s, 0x%x\n", _r3, _r2, sa);
+   }
+
+   if (func == 0x4) //sll
+   {
+       if ( r2 == -1 || r3 == -1 || r1 == -1) //check if invalid register
+            return;
+
+       PRINT_ADDRESS("\t");
+       printf("sllv\t%s, %s, %s\n", _r3, _r2, _r1);
+   }
+
+   if (func == 0x6) //sll
+   {
+       if ( r2 == -1 || r3 == -1 || r1 == -1) //check if invalid register
+            return;
+
+       PRINT_ADDRESS("\t");
+       printf("slrv\t%s, %s, %s\n", _r3, _r2, _r1);
+   }
+
+   if (func == 0x7) //sll
+   {
+       if ( r2 == -1 || r3 == -1 || r1 == -1) //check if invalid register
+            return;
+
+       PRINT_ADDRESS("\t");
+       printf("srav\t%s, %s, %s\n", _r3, _r2, _r1);
+   }
+
+   if (func == 0x8) //sll
+   {
+       if (r1 == -1) //check if invalid register
+            return;
+
+       PRINT_ADDRESS("\t");
+       printf("jr\t%s\n", _r1);
+   }
+
+   if (func == 0x9) //sll
+   {
+       if (r1 == -1 || _r3 == -1) //check if invalid register
+            return;
+
+       PRINT_ADDRESS("\t");
+       printf("jalr\t%s, %s\n", _r3, _r1);
+   }
+
 
 
 
 }
+
 
 
 int main(void)
@@ -74,34 +190,35 @@ int main(void)
     FILE *fptr;
 
     fptr = fopen("/home/ali/Downloads/lua","rb");
-    char flag =0;
-    int tt = 0x400000, counter = 0;
-
-    char r1,r2,r3;
 
 
-    char _r1[4] = {0}, _r2[4] = {0}, _r3[4] = {0};
+
+
+
 
     while(1)
     {
         if (fread(&x,1, 4, fptr) == 0)
             break;
 
+        address+=4;
 
 
 
+        if (getnbits(x,32,6) == 0)
+        {
+            parse_r(x);
 
-        tt+=4;
+        }
 
-
-
-        if (getnbits(x,32,6) == 0 && getnbits(x,6,6) == 0b100001){
+        /*if (getnbits(x,32,6) == 0 && getnbits(x,6,6) == 0b100001){
 
 
 
             r3 = getnbits(x,16,5);
             r2 = getnbits(x,21,5);
             r1 = getnbits(x,26,5);
+
 
 
             detect_register(_r1,r1);
@@ -112,7 +229,7 @@ int main(void)
 
 
 
-            printf("%x:\t",tt-4);
+            printf("%x:\t",address-4);
 
             printf("%s ", "addu");
 
@@ -121,23 +238,18 @@ int main(void)
             printf("%s\n", _r2);
 
 
-
-
-
             /*
-            printf("\n--- %x ---\n",tt-4);
-
-            printf("%s\t%x\n",getstrbits(x,32,32),x);
-
             printf("%s\t%x\n",getstrbits(x,6,6),getnbits(x,6,6));
             printf("%s\t%x\n",getstrbits(x,11,5),getnbits(x,11,5));
             printf("%s\t%x\n",getstrbits(x,16,5),getnbits(x,16,5));
             printf("%s\t%x\n",getstrbits(x,21,5),getnbits(x,21,5));
             printf("%s\t%x\n",getstrbits(x,26,5),getnbits(x,26,5));
-            printf("%s\t%x\n",getstrbits(x,32,6),getnbits(x,32,6));*/
-            counter++;
+            printf("%s\t%x\n",getstrbits(x,32,6),getnbits(x,32,6));
+            *//*
+
 
         }
+        */
 
 
 
@@ -145,19 +257,9 @@ int main(void)
 
 
 
-        /*
-        printf("%s\t%02x\n",getstrbits(x,8,8),x);
-        printf("%s\t%02x\n",getstrbits(x,3,3),x);
-
-        printf("%s\t%d\n",getstrbits(getnbits(x, 3, 3),32,32),getnbits(x, 3, 3));
-
-        //printf("%s\n",getstrbits(SWAP_INT32(x),32,32));
-
-        getchar();*/
 
     }
     fclose(fptr);
-    printf("\n--- %d ---\n",counter);
     return 0;
 }
 
